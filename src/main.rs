442980020006869d4ff1cf8b85c7945cebcf6d03
SirHowitzer <aragosaurus@tuta.io>
2026-01-04 00:28:54 +0700

    Added Basic Bevy Feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::{camera::ScalingMode, prelude::*};
+use bevy::window::{Window, WindowResolution, WindowPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin { //GAME WINDOW SETTINGS
+                    primary_window: Some(Window {
+                        title: "Toy RTS RTS-Controlling System".to_string(),
+                        resolution: WindowResolution::new(1600, 900),
+                        resizable: false,
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()) //PIXEL PERFECT 2D TEXTURES RENDERER
+        )
+        .add_systems(Startup, str_game_init)
+        .add_systems(Update, (udt_rts_controlling_system))
+        .run();
 }
+
+fn str_game_init(mut commands: Commands,asset_server: Res<AssetServer>){
+    
+}
+
+fn udt_rts_controlling_system(mut commands: Commands,asset_server: Res<AssetServer>){
+    
+}
+
