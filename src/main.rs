96bf98c812b2b56e02cf8212c82f02bf3ce6cb9c
sirhowitzer <odeemfal@gmail.com>
2026-01-04 03:43:16 +0700

    udt_rts_controlling_system now detects if mouse is pressed/dragging, it also logs cursor world position

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use bevy::{camera::ScalingMode, prelude::*};
+use bevy::{camera::*, prelude::*};
 use bevy::window::{Window, WindowResolution, WindowPlugin};
+//use bevy_cursor::prelude::*;
 
 fn main() {
     App::new()
@@ -16,16 +17,42 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()) //PIXEL PERFECT 2D TEXTURES RENDERER
         )
+        //.add_plugins(TrackCursorPlugin)
         .add_systems(Startup, str_game_init)
-        .add_systems(Update, (udt_rts_controlling_system))
+        .add_systems(Update, udt_rts_controlling_system)
         .run();
 }
 
-fn str_game_init(mut commands: Commands,asset_server: Res<AssetServer>){
-    
+fn str_game_init(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    let shape = meshes.add(Segment2d::new(
+        Vec2::new(-50.0, 50.0),
+        Vec2::new(50.0, -50.0),
+    ));
+    const CIRCLE_COLOR: bevy::prelude::Color = Color::hsl(0.0, 0.0, 1.0);
+    commands.spawn((
+        Mesh2d(shape),
+        MeshMaterial2d(materials.add(CIRCLE_COLOR)),
+        Transform::from_xyz(0., 0., 0.),
+    ));
 }
 
-fn udt_rts_controlling_system(mut commands: Commands,asset_server: Res<AssetServer>){
-    
+fn udt_rts_controlling_system(mut commands: Commands, buttons: Res<ButtonInput<MouseButton>>, window: Single<&Window>, camera_query: Single<(&Camera, &GlobalTransform)>){
+    if buttons.pressed(MouseButton::Left) {
+        println!("DRAGGING");
+
+        let (camera, camera_transform) = *camera_query;
+        
+        let cursor_position = window.cursor_position().unwrap();
+
+        let cursor_world_position = camera.viewport_to_world_2d(camera_transform, cursor_position);
+        
+        println!("Cursor position: {cursor_world_position:?}");
+    }
 }
 
